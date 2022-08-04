da37bdbd805e9259d8f69fdecda2d89d0727ebd0
siuDongMac <shaodong@shaoDongdeMacBook-Pro.local>
2022-08-04 23:24:44 +0800

    feat: api mock - stage one

--- app.js
+++ app.js
@@ -1,10 +1,28 @@
+const fs = require('fs');
+const path = require('path');
 const express = require('express');
+const chokidar = require('chokidar');
 
+const mocker = {};
 const app = express();
 
-app.get('/api/data', (req, res) => {
-    res.send('hello world');
-})
+const apiMock = (app, dir) => {
+    app.all('/*', (req, res, next) => {
+        const key = req.originalUrl;
+        if (mocker[key]) {
+            return res.send(mocker[key]);
+        }
+        next();
+    });
+    chokidar.watch(dir).on('all', (event, curPath) => {
+        if (fs.statSync(curPath).isFile()) {
+            delete require.cache[path.resolve(curPath)];
+            Object.assign(mocker, require(curPath));
+        }
+    })
+}
+
+apiMock(app, path.resolve('./mock'));
 
 app.listen(3000, () => {
     console.log('listening at 3000');
---
+++ mock/user.js
@@ -0,0 +1,6 @@
+module.exports = {
+    '/api/users': [
+        { id: 1, name: 'user one'},
+        { id: 2, name: 'user two'},
+    ]
+}
