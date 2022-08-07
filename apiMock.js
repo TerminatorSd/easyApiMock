72d2a6a4c71ae720a39fe7e5c1fccf0feb3ae2ab
siudong <553828127@qq.com>
2022-08-07 19:24:12 +0800

    feat: 支持删除文件

--- apiMock.js
+++ apiMock.js
@@ -1,7 +1,9 @@
 const fs = require("fs");
 const path = require("path");
 const chokidar = require("chokidar");
+
 const mocker = {};
+const cacheUrlInFile = {};
 
 module.exports.apiMock = (app, dir) => {
   app.all("/*", (req, res, next) => {
@@ -12,9 +14,22 @@ module.exports.apiMock = (app, dir) => {
     next();
   });
   chokidar.watch(dir).on("all", (event, curPath) => {
-    if (fs.statSync(curPath).isFile()) {
-      delete require.cache[path.resolve(curPath)];
-      Object.assign(mocker, require(curPath));
+    if (event === "unlink") {
+      console.log("删除mock文件：", curPath);
+      const toDeleteArr = cacheUrlInFile[curPath];
+      toDeleteArr.forEach((item) => {
+        delete mocker[item];
+      });
+    } else if (["add", "change"].includes(event)) {
+      if (fs.statSync(curPath).isFile()) {
+        console.log(`${event === "add" ? "新增" : "修改"}mock文件：`, curPath);
+        delete require.cache[path.resolve(curPath)];
+        const module = require(curPath);
+        Object.assign(mocker, module);
+        Object.assign(cacheUrlInFile, {
+          [curPath]: Object.keys(module),
+        });
+      }
     }
   });
 };
